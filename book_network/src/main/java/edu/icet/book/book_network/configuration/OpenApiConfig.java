39e8bd25b4c51ab0c3ff13b0af462b7fa071fe79
thisaramax20 <thisarathejana20@gmail.com>
2025-03-15 20:36:29 +0530

    add swagger API documentation

---
+++ book_network/src/main/java/edu/icet/book/book_network/configuration/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package edu.icet.book.book_network.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book Network API",
+                version = "1.0",
+                description = "Book Network API"
+        ),
+        servers = @Server(
+                description = "Local Environment",
+                url = "http://localhost:8080/api/v1"
+        ),
+        security = {
+                @SecurityRequirement(
+                        name = "Bearer Authentication"
+                )
+        }
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
